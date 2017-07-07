38b0147a1d371add4090c7682a1e67dd7cabc3c1
Aghaahmadi <ahmadi@gmail.com>
2017-07-07 18:13:48 +0430

    Read all paragraph strings

--- Main.py
+++ Main.py
@@ -1,9 +1,15 @@
+# https://www.youtube.com/watch?v=aIPqt-OdmS0
+
 import bs4 as bs
 import urllib.request
 
 sauce = urllib.request.urlopen('http://www.asriran.com/fa/news/547597').read()
 soup = bs.BeautifulSoup(sauce,'lxml')
 
-# print(sauce)
-print(soup.title.string)
-# print(soup.p.string)
+# print(soup.title.string)
+
+for paragraph in soup.find_all('div'):
+	s = paragraph.string
+	if s == None:
+		continue
+	print(paragraph.string)
