8807ca239855b437c4509c08367f85bd0148e4ea
Aghaahmadi <ahmadi@gmail.com>
2017-07-14 21:41:20 +0430

    use pandas for read table from webpages

--- p3.py
+++ p3.py
@@ -2,16 +2,21 @@
 # https://www.youtube.com/watch?v=sAuGH1Kto2I&feature=share
 import bs4 as bs
 import urllib.request
+import pandas as pd
 
 # sauce = urllib.request.urlopen('https://play.google.com/store/apps/details?id=com.anydo&hl=en').read()
-sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
-soup = bs.BeautifulSoup(sauce,'lxml')
+# sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+# soup = bs.BeautifulSoup(sauce,'lxml')
 
-table = soup.find('table')
+# table = soup.find('table')
 
-table_rows = table.find_all('tr')
+# table_rows = table.find_all('tr')
 
-for tr in table_rows:
-	td = tr.find_all('td')
-	row = [i.text for i in td]
-	print(row)
+# for tr in table_rows:
+# 	td = tr.find_all('td')
+# 	row = [i.text for i in td]
+# 	print(row)
+
+dfs = pd.read_html('https://pythonprogramming.net/parsememcparseface/')
+for df in dfs:
+	print(df)
